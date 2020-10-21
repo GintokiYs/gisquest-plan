60bc6b26d4bc32c7198dee67923a7f98d446c45d
dingyf <410956232@qq.com>
2020-10-21 11:14:39 +0800

    指标设计数据提交

--- src/main/java/com/gisquest/plan/service/dao/TargetDesignMapper.java
+++ src/main/java/com/gisquest/plan/service/dao/TargetDesignMapper.java
@@ -3,7 +3,15 @@ package com.gisquest.plan.service.dao;
 import com.gisquest.plan.service.model.targetDesign.TargetDesign;
 
 public interface TargetDesignMapper {
+    int deleteByPrimaryKey(String id);
+
     int insert(TargetDesign record);
 
     int insertSelective(TargetDesign record);
+
+    TargetDesign selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(TargetDesign record);
+
+    int updateByPrimaryKey(TargetDesign record);
 }
