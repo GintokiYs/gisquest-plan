59a557fd768c1420cfc12e921dcab1ca0d9fa737
dingyf <410956232@qq.com>
2020-10-22 20:16:56 +0800

    Tag代码生成提交

--- src/main/java/com/gisquest/plan/service/model/tag/Tag.java
+++ src/main/java/com/gisquest/plan/service/model/tag/Tag.java
@@ -3,11 +3,19 @@ package com.gisquest.plan.service.model.tag;
 public class Tag {
     private String id;
 
-    private String areaType;
-
     private String targetId;
 
     private String indexId;
+
+    private String areaType;
+
+    private Integer year;
+
+    private Long target;
+
+    private Integer sourceYear;
+
+    private Integer alias;
 
     public String getId() {
         return id;
@@ -15,14 +23,6 @@ public class Tag {
 
     public void setId(String id) {
         this.id = id == null ? null : id.trim();
-    }
-
-    public String getAreaType() {
-        return areaType;
-    }
-
-    public void setAreaType(String areaType) {
-        this.areaType = areaType == null ? null : areaType.trim();
     }
 
     public String getTargetId() {
@@ -39,5 +39,45 @@ public class Tag {
 
     public void setIndexId(String indexId) {
         this.indexId = indexId == null ? null : indexId.trim();
+    }
+
+    public String getAreaType() {
+        return areaType;
+    }
+
+    public void setAreaType(String areaType) {
+        this.areaType = areaType == null ? null : areaType.trim();
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Long getTarget() {
+        return target;
+    }
+
+    public void setTarget(Long target) {
+        this.target = target;
+    }
+
+    public Integer getSourceYear() {
+        return sourceYear;
+    }
+
+    public void setSourceYear(Integer sourceYear) {
+        this.sourceYear = sourceYear;
+    }
+
+    public Integer getAlias() {
+        return alias;
+    }
+
+    public void setAlias(Integer alias) {
+        this.alias = alias;
     }
 }
